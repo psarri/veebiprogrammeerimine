bce32c4a98362d416325503d8bf1867ba7816fbe
Petrik Sarri <psarri@users.noreply.github.com>
2017-09-11 10:37:04 +0300

    HTML koodile lisatud esimene php osa

---
+++ esimene.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf8">
+	<title>Petrik veebiprogemise asjad</title>
+</head>
+<body>
+	<h1>Petrik</h1>
+	<p>See veebileht on loodud veebiprogrammerimise kursusel ning ei sisalda tõsiselt võetavat sisu.</p>
+
+	<p>Tere tulemast minu veebilehele!</p>
+
+	<?php
+		echo"<p>Täna on vastik ilm.</p>";
+	
+	?>
+	
+</body>
+
+</html>
