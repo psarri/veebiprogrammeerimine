1d3b7c1eeeaae34e4d7b4031264c09914ed7c4a4
Petrik Sarri <psarri@users.noreply.github.com>
2017-09-18 11:32:30 +0300

    juhusliku pildi lisamine

---
+++ tund3/foto.php
@@ -0,0 +1,41 @@
+<?php
+	$dirToRead = "../../pics/";
+	//kuna tahan ainult pildifaile, siis filtreerin
+	$picFileTypes = ["jpg", "jpeg", "png", "gif"];
+	$picFiles = [];
+	//$allFiles = scandir($dirToRead);
+	//loen kataloogi ja viskan kaks  esimest massiivi liiget (. ja ..)
+	//välja
+	$allFiles = array_slice(scandir($dirToRead),2);
+	//var_dump($allFiles);
+	
+	//tsükkel, mis töötab ainult massiividega
+	foreach ($allFiles as $file){
+		$fileType = pathinfo($file, PATHINFO_EXTENSION);
+		//kas see tüüp on lubatud nimekirjas
+		if (in_array($picFiles, $picFileTypes) == true){
+			array_push($picFiles, $file);
+		}
+	}//foreach lõppeb
+	var_dump($allFiles);
+	
+	//mitu pilti on?
+	$fileCount = count($picFiles);
+	$picNumber = mt_rand(0, $fileCount -1);
+	$picToShow = $picFiles[$picNumber];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf8">
+	<title>Petriku veebiprogemise asjad</title>
+</head>
+<body>
+	<h1>Petrik</h1>
+	<p>See veebileht on loodud veebiprogrammerimise kursusel ning ei sisalda tõsiselt võetavat sisu.</p>
+
+	<p>Üks pilt Tallinna Ülikoolist!</p>
+	<img src="<?php echo $dirToRead .$picToShow;?>" alt="Tallinna Ülikool">
+</body>
+</html>
